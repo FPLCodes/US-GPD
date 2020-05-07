29cea8d88827b438f99f8dc095655873c5c2591d
FPLCodes <minecraftdudesh@gmail.com>
2020-05-07 15:45:19 +0800

    Updated

--- src/components/Array.js
+++ src/components/Array.js
@@ -1,49 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "../App.css";
 import * as d3 from "d3";
 
 export default function Array() {
-  const [dataset, setDataset] = useState([12, 31, 22, 17, 25, 18, 29, 14, 9]);
   const w = 500;
   const h = 100;
 
-  const svg = d3
-    .select("#root")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
-
-  svg
-    .selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => i * 25)
-    .attr("y", (d) => h - 3 * d)
-    .attr("width", 20)
-    .attr("height", (d) => 3 * d);
-
-  function handleClick() {
-    let data = [...dataset];
-
-    for (let i = 0; i < data.length; i++) {
-      for (let j = 0; j < data.length; j++) {
-        if (data[j] > data[j + 1]) {
-          const temp = data[j];
-          data[j] = data[j + 1];
-          data[j + 1] = temp;
-        }
-      }
-    }
-    setDataset(data);
+  let arr = [];
+  for (let i = 0; i < 9; i++) {
+    arr.push(Math.round(Math.random() * 35));
   }
+
+  const [dataset, setDataset] = useState(arr);
+  const [count, setCount] = useState(dataset.length);
+  const svgRef = useRef();
+
+  useEffect(() => {
+    if (count > 0) {
+      let data = [...dataset];
+
+      for (let i = 0; i < data.length; i++) {
+        setTimeout(() => {
+          for (let j = 0; j < data.length; j++) {
+            if (data[j] > data[j + 1]) {
+              let temp = data[j];
+              data[j] = data[j + 1];
+              data[j + 1] = temp;
+              console.log(data);
+            }
+          }
+          setDataset(data);
+          d3.selectAll("rect").remove();
+        }, i * 100);
+      }
+      setDataset(data);
+      d3.selectAll("rect").remove();
+      setCount(count - 1);
+    }
+
+    const svg = d3.select(svgRef.current);
+    svg
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h)
+      .selectAll("rect")
+      .data(dataset)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 25)
+      .attr("y", (d) => h - 3 * d)
+      .attr("width", 20)
+      .attr("height", (d) => 3 * d);
+  }, [dataset]);
 
   return (
     <div>
-      <svg />
-      <button className="button" onClick={handleClick}>
-        Sort
-      </button>
+      <svg ref={svgRef} />
     </div>
   );
 }
