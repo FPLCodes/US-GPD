e877f2a2dda93203ae3d6523e0dafd699d4939f4
FPLCodes <minecraftdudesh@gmail.com>
2020-05-09 15:50:29 +0800

    Added mouseover features

--- src/components/Array.js
+++ src/components/Array.js
@@ -1,14 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import "../App.css";
-import {
-  select,
-  scaleLinear,
-  scaleTime,
-  axisBottom,
-  axisLeft,
-  max,
-  min,
-} from "d3";
+import * as d3 from "d3";
 
 export default function Array() {
   const [dataset, setDataset] = useState([]);
@@ -29,18 +21,21 @@ export default function Array() {
   useEffect(() => {
     const time = dataset.map((item) => new Date(item[0]));
 
-    const maxDate = new Date(max(time));
+    const maxDate = new Date(d3.max(time));
     maxDate.setMonth(maxDate.getMonth() + 3);
 
-    const xScale = scaleTime()
-      .domain([min(time), maxDate])
+    const xScale = d3
+      .scaleTime()
+      .domain([d3.min(time), maxDate])
       .range([0, w + 1]);
 
-    const yScale = scaleLinear()
-      .domain([0, max(dataset, (d) => d[1])])
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(dataset, (d) => d[1])])
       .range([h, 0]);
 
-    const svg = select(svgRef.current)
+    const svg = d3
+      .select(svgRef.current)
       .append("svg")
       .attr("width", w)
       .attr("height", h);
@@ -52,25 +47,44 @@ export default function Array() {
       .append("rect")
       .attr("x", (d, i) => xScale(time[i]))
       .attr("y", -h)
+      .attr("width", 3.2)
       .style("transform", "scale(1, -1)")
+      .attr("class", "bar")
+      .attr("fill", "skyblue")
+      .on("mouseover", (d, i) => {
+        svg
+          .selectAll(".tooltip")
+          .data([d])
+          .join((enter) => enter.append("text"))
+          .attr("class", "tooltip")
+          .text(`${d[0]}`)
+          .attr("x", 60)
+          .attr("y", 50)
+          .attr("opacity", 1)
+          .attr("font-size", 22)
+          .attr("font-family", "Jost");
+        svg
+          .selectAll(".tooltip1")
+          .data([d])
+          .join((enter) => enter.append("text"))
+          .attr("class", "tooltip")
+          .attr("x", 60)
+          .attr("y", 80)
+          .text(`$${d[1]} Billion`)
+          .attr("opacity", 1)
+          .attr("font-size", 22)
+          .attr("font-family", "Jost");
+      })
       .transition()
       .duration(1500)
-      .attr("height", (d) => h - yScale(d[1]))
-      .attr("width", 3)
-      .attr("fill", "skyblue")
-      .attr("class", "bar");
+      .attr("height", (d) => h - yScale(d[1]));
 
-    svg
-      .selectAll("rect")
-      .append("title")
-      .text((d) => `${d[0]}\n$${d[1]} Billion`);
-
-    const xAxis = axisBottom(xScale);
-    const yAxis = axisLeft(yScale);
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
 
     svg.append("g").attr("transform", `translate(0, ${h})`).call(xAxis);
     svg.append("g").attr("transform", `translate(-1, 0)`).call(yAxis);
-  });
+  }, [dataset]);
 
   return (
     <div className="container">
